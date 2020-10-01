369cc5ace4c53930ff9962df72170a7c152d6ea0
github username <github email address>
2020-10-01 17:21:34 -0600

    Needs more on rendering likes

--- login.js
+++ login.js
@@ -9,8 +9,6 @@ const getUsers = document.querySelector('.get-users')
 const createUserForm = document.querySelector('.create-user')
 
 
-
-
 loginForm.addEventListener('submit', loginUser)
 createUserForm.addEventListener('submit', createNewUser)
 
@@ -36,13 +34,16 @@ function createNewUser(event){
             if (user.errors){
                 throw new Error(user.errors[0])
             }
-                //if redirecting 
-            localStorage.setItem('token', user.token)
-            window.location.href = '/usertrees.html'
+            console.log(user.user.id)
 
+            // const previouslyLiked = user.users.find(user => user.id === parseInt(localStorage.user_id))
+            localStorage.setItem('token', user.token)
+            localStorage.setItem('user id', user.id)
+            window.location.href = `/usertrees.html?id=${user.user.id}`
+            
             // if not redirecting 
 
-            // console.log('Success!')
+            console.log('Success!')
             // const errorMessage = createUserForm.querySelector(".create-error-message")
             // errorMessage.textContent = 'New user created, now login!'
             // errorMessage.classList.remove('hidden')
@@ -51,7 +52,7 @@ function createNewUser(event){
         })
         .catch(handleError)
     
-        createUserForm.reset()   
+        
 }
 
 // function handleResponseErrors(response){
@@ -70,22 +71,22 @@ function handleError(error){
     errorMessage.classList.remove('hidden')
 }
 
-function handleFav (event){
-    const tree_id = event.target.dataset.treeId
-    const user_id = localStorage.getItem('user_id')
+// function handleFav (event){
+//     const tree_id = event.target.dataset.treeId
+//     const user_id = localStorage.getItem('user_id')
 
-    fetch(joinURL, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-        body: JSON.stringify({user_id, tree_id})
-    })
-    .then(response => response.json())
-    .then(result => console.log('fav result', result))
+//     fetch(joinURL, {
+//         method: 'POST',
+//         headers: {
+//             'Content-type': 'application/json',
+//             'Authorization': `Bearer ${localStorage.getItem('token')}`
+//         },
+//         body: JSON.stringify({user_id, tree_id})
+//     })
+//     .then(response => response.json())
+//     .then(result => console.log('fav result', result))
 
-}
+// }
 
 function loginUser(event){
     event.preventDefault()
@@ -108,56 +109,56 @@ function loginUser(event){
         localStorage.setItem('user_id', result.user.id)
     })
 
-    loginForm.reset()
+    // loginForm.reset()
 
-    callTrees(event, username)
+    // callTrees(event, username)
 }
 
-function callTrees(event, username){
-fetch('http://localhost:3000/trees')
-    .then(response => response.json())
-    .then(trees => {
-        trees.forEach(tree => {
+// function callTrees(event, username){
+// fetch('http://localhost:3000/trees')
+//     .then(response => response.json())
+//     .then(trees => {
+//         trees.forEach(tree => {
 
-    let card = document.createElement('div')
-    card.classList = ('card')
-    card.innerHTML = `
-            <h5 class="card-title">${tree.name}</h5>
-                <h6 class="card-sciname">${tree.scientific_name}</h6>
-                    <img src="${tree.image}"  alt="Tree Picture" style="width:100%"></img>
-                    <div class="card-body">
-                        <p class="card-information"><strong>Bark: </strong> ${tree.bark} 
-                        <br> <strong>Leaves: </strong> ${tree.leaves} 
-                        <br> <strong>Fruit:</strong> ${tree.fruit}
-                        <br> <strong>Elevation: </strong> ${tree.elevation_max}ft to ${tree.elevation_min}ft
-                        <br> <strong>Height: </strong> ${tree.height_max}ft to ${tree.height_min}ft
-                        <br> <strong>Tree Trivia: </strong> ${tree.fun_fact}</p>
+//     let card = document.createElement('div')
+//     card.classList = ('card')
+//     card.innerHTML = `
+//             <h5 class="card-title">${tree.name}</h5>
+//                 <h6 class="card-sciname">${tree.scientific_name}</h6>
+//                     <img src="${tree.image}"  alt="Tree Picture" style="width:100%"></img>
+//                     <div class="card-body">
+//                         <p class="card-information"><strong>Bark: </strong> ${tree.bark} 
+//                         <br> <strong>Leaves: </strong> ${tree.leaves} 
+//                         <br> <strong>Fruit:</strong> ${tree.fruit}
+//                         <br> <strong>Elevation: </strong> ${tree.elevation_max}ft to ${tree.elevation_min}ft
+//                         <br> <strong>Height: </strong> ${tree.height_max}ft to ${tree.height_min}ft
+//                         <br> <strong>Tree Trivia: </strong> ${tree.fun_fact}</p>
                         
-                        <br> <label for="comment">Comment:</label>
-                        <br> <textarea name="comment" rows="10" cols="25">Add your comments on this tree here.</textarea>
-                        </div>`
-    const favButton = document.createElement('button')
-    favButton.textContent = "⭐"  
+//                         <br> <label for="comment">Comment:</label>
+//                         <br> <textarea name="comment" rows="10" cols="25">Add your comments on this tree here.</textarea>
+//                         </div>`
+//     const favButton = document.createElement('button')
+//     favButton.textContent = "⭐"  
     
-    const previouslyLiked = tree.users.find(user => user.id === parseInt(localStorage.user_id))      
+//     const previouslyLiked = tree.users.find(user => user.id === parseInt(localStorage.user_id))      
     
-    if(previouslyLiked){
-        console.log(previouslyLiked)
-        favButton.classList.add('active-favorite')
-    } else {
-        favButton.classList.add('fav-button')
-    } 
+//     if(previouslyLiked){
+//         console.log(previouslyLiked)
+//         favButton.classList.add('active-favorite')
+//     } else {
+//         favButton.classList.add('fav-button')
+//     } 
 
-    favButton.dataset.treeId = tree.id
+//     favButton.dataset.treeId = tree.id
 
-    card.append(favButton)
-    treeCardContainer.append(card)
+//     card.append(favButton)
+//     treeCardContainer.append(card)
     
-    favButton.addEventListener('click', (event) =>{ 
-        favButton.classList.toggle("activeFavorite")
-        handleFav(event)
-    })
+//     favButton.addEventListener('click', (event) =>{ 
+//         favButton.classList.toggle("activeFavorite")
+//         handleFav(event)
+//     })
 
-})
-    })
-}
+// })
+//     })
+// }
--- usertrees.js
+++ usertrees.js
@@ -1,5 +1,16 @@
+const searchParams = new URLSearchParams(window.location.search)
+const id = searchParams.get('id')
+console.log(id)
+
+
 const baseURL = 'http://localhost:3000'
 const usertreesURL =`${baseURL}/usertrees`
+const userIdURL = `${baseURL}/login`
+const usersURL = `${baseURL}/users`
+const joinURL = `${baseURL}/trees_users`
+
+const treeCardContainer = document.querySelector('.card-container')
+
 console.log(localStorage.getItem('token'))
 
 fetch(usertreesURL, {
@@ -9,4 +20,93 @@ fetch(usertreesURL, {
         'Authorization' : `Bearer ${localStorage.getItem('token')}`
     }
 }).then(response => response.json())
-    .then(result => console.log("user", result))
+    .then(result => {
+
+        localStorage.setItem('user_id', result.id)
+
+        // console.log("user", result)
+        // console.log("this",result)
+        callTrees()
+    
+    })
+
+function callTrees(){
+fetch('http://localhost:3000/trees')
+    .then(response => response.json())
+    .then(trees => {
+        trees.forEach(tree => {
+
+    let card = document.createElement('div')
+    card.classList = ('card')
+    card.innerHTML = `
+            <h5 class="card-title">${tree.name}</h5>
+                <h6 class="card-sciname">${tree.scientific_name}</h6>
+                    <img src="${tree.image}"  alt="Tree Picture" style="width:100%"></img>
+                    <div class="card-body">
+                        <p class="card-information"><strong>Bark: </strong> ${tree.bark} 
+                        <br> <strong>Leaves: </strong> ${tree.leaves} 
+                        <br> <strong>Fruit:</strong> ${tree.fruit}
+                        <br> <strong>Elevation: </strong> ${tree.elevation_max}ft to ${tree.elevation_min}ft
+                        <br> <strong>Height: </strong> ${tree.height_max}ft to ${tree.height_min}ft
+                        <br> <strong>Tree Trivia: </strong> ${tree.fun_fact}</p>
+                        
+                        <br> <label for="comment">Comment:</label>
+                        <br> <textarea name="comment" rows="10" cols="25">Add your comments on this tree here.</textarea>
+                        </div>`
+    const favButton = document.createElement('button')
+    favButton.textContent = "⭐"  
+    
+
+    fetch(joinURL, {
+            method: "GET",
+            headers:{
+                'Content-type': 'application/json',
+                'Authorization' : `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(response => response.json())
+        .then(result => {
+            result
+            
+            
+    // const treePair = tree.users.find(user => user.id === id)
+    const previouslyLiked = result.id      
+    console.log(result)
+    if(previouslyLiked){
+        favButton.classList.add('active-favorite')
+    } else {
+        favButton.classList.add('fav-button')
+    } 
+    })
+    favButton.dataset.treeId = tree.id
+
+    card.append(favButton)
+    treeCardContainer.append(card)
+    
+    favButton.addEventListener('click', (event) =>{ 
+        console.log("clicked")
+        favButton.classList.toggle("active-favorite")
+        handleFav(event)
+    })
+
+})
+    })
+}
+
+
+function handleFav (event){
+    const tree_id = event.target.dataset.treeId
+    const user_id = id
+    console.log(user_id)
+    fetch(joinURL, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({user_id, tree_id})
+    })
+    .then(response => response.json())
+    .then(result => console.log('fav result', result))
+
+}
